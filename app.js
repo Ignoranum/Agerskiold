1656f28adc5de5b8e35ac5c921d208e1fcf78fdf
Ignoranum <61806026+Ignoranum@users.noreply.github.com>
2024-10-06 14:38:04 +0200

    Added sqlite3 server

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import server from './tcp-server.js';
+import db from './db.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,6 +15,7 @@ let app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use("/", express.static('./public'));
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
@@ -21,6 +23,39 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
     res.sendFile(__dirname + "/public/about.html");
+});
+
+// user manipulation
+app.get("/api/getUsers", (req, res) => {
+    if (!req.query.username) {
+        res.status(400).json({ error: "Invalid request" });
+        return;
+    }
+    
+    db.all(`SELECT * FROM users;`, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json(rows);
+    });
+});
+
+app.post("/api/addUser", (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        res.status(400).json({ error: "Invalid request" });
+        return;
+    }
+    let { username, password } = req.body;
+    
+    db.all(`INSERT INTO users (username, password)
+            VALUES(?, ?);`, [username, password], (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json(rows);
+    });
 });
 
 app.all('*', (req, res) => {
---
+++ scripts/dump.sql
@@ -0,0 +1,9 @@
+INSERT INTO customers (username, email, password) VALUES ('hans', 'hanshansen@gmail.com', 'hansemanse');
+INSERT INTO customers (username, email, password) VALUES ('jens', 'hanshansen@gmail.com', 'hansemanse');
+
+INSERT INTO products (productName, imgsrc) VALUES ('Orange Juice', 'orange_juice.jpg');
+INSERT INTO products (productName, imgsrc) VALUES ('Apple Juice', 'apple_juice.jpg');
+INSERT INTO products (productName, imgsrc) VALUES ('Grape Juice', 'grapes.jpg');
+INSERT INTO products (productName, imgsrc) VALUES ('Pineapple Juice', 'pineapple_juice.jpg');
+INSERT INTO products (productName, imgsrc) VALUES ('Espresso', 'espresso.jpg');
+INSERT INTO products (productName, imgsrc) VALUES ('Cappuccino', 'cappuccino.jpg');
