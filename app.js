dea4334c62d1f71d9e7ec5cad222803db07aa266
Ignoranum <61806026+Ignoranum@users.noreply.github.com>
2024-10-06 12:19:28 +0200

    Newer v, test

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = 3000;
 let app = express();
@@ -8,6 +13,14 @@ let app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use("/", express.static('./public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/public/about.html");
+});
 
 app.all('*', (req, res) => {
     res.redirect("/");
